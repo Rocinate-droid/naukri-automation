eb85cef693de32e67394eb302e5850fcb50f6572
sreejith <sreejithcs274@gmail.com>
2025-07-21 07:12:42 +0000

    jenkins credentials added

--- src/test/java/com/myproject/AppTest.java
+++ src/test/java/com/myproject/AppTest.java
@@ -29,20 +29,19 @@ public class AppTest
 
     @Test
     public void login(){
+	String email = System.getProperty("email");
+	String password = System.getProperty("password");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.partialLinkText("Login")));
         driver.findElement(By.partialLinkText("Login")).click();
         WebElement email = driver.findElement(By.xpath("//input[@type=\"text\"][@placeholder=\"Enter your active Email ID / Username\"]"));
         WebElement password = driver.findElement(By.cssSelector("input[type=\"password\"]"));
-        email.sendKeys("sreejithcs895@gmail.com");
-        password.sendKeys("Ancallagon99#");
+        email.sendKeys(email);
+        password.sendKeys(password);
         driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
         driver.findElement(By.cssSelector("div[class=\"view-profile-wrapper\"]")).click();;
         WebElement uploadbutton = driver.findElement(By.cssSelector("input[type=\"file\"][id=\"attachCV\"]"));
         uploadbutton.sendKeys("/mnt/d/Resume (2).pdf");
-
-
-
     }
 
 }
